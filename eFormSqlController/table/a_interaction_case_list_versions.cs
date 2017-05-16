0253adb89bca070e1ea95fe69a2877b12dc459d2
TroelsMA <Markvard24@hotmail.com>
2017-05-16 12:49:37 +0200

    added versions to Interaction table

---
+++ eFormSqlController/table/a_interaction_case_list_versions.cs
@@ -0,0 +1,38 @@
+namespace eFormSqlController
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class a_interaction_case_list_versions
+    {
+        [Key]
+        public int id { get; set; }
+
+        public int? a_interaction_case_list_version_id { get; set; }
+
+        [StringLength(255)]
+        public string workflow_state { get; set; }
+
+        public int? version { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        public DateTime? created_at { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        public DateTime? updated_at { get; set; }
+
+        public int? siteId { get; set; }
+
+        public string stat { get; set; }
+
+        public string microting_uid { get; set; }
+
+        public string check_uid { get; set; }
+
+        public int? case_id { get; set; }
+
+    }
+}
