550e36473bbfe2ab0baaf8070da9f45e58423e52
René Schultz Madsen <rm@microting.com>
2017-08-22 13:10:45 +0200

    Fixing to use the correct project type.

--- eFormSDK.Tests/UnitTest1.cs
+++ eFormSDK.Tests/UnitTest1.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace eFormSDK.Tests
 {
-    class Tests
+    public class UnitTest1
     {
         [Fact]
         public void FirstTest()
