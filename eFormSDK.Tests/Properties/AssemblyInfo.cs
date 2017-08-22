ca6748baa670c4c6703dc4dc219aa7ba2248b3b4
René Schultz Madsen <rm@microting.com>
2017-08-22 10:49:54 +0200

    Adding the first steps to make use of Travis CI.

---
+++ eFormSDK.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("eFormSDK.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("eFormSDK.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("63e5a140-5f00-480e-81bb-7f67cf2517e9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ eFormSDK.Tests/Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace eFormSDK.Tests
+{
+    class Tests
+    {
+        [Fact]
+        public void FirstTest()
+        {
+            bool value = true;
+
+            Assert.Equal(true, value);
+        }
+    }
+}
