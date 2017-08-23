6f696c5fe1aca9af13087f521851f4f8977c4592
TroelsMA <Markvard24@hotmail.com>
2017-08-23 14:19:55 +0200

    Travis fix ?

--- UnitTest/SDK.cs
+++ UnitTest/SDK.cs
@@ -8,39 +8,83 @@ namespace UnitTest
 {
     public class SDK
     {
+        string serverConnectionString = "Data Source=DESKTOP-7V1APE5\\SQLEXPRESS;Initial Catalog=MicrotingTest;Integrated Security=True";
+        int siteId1 = 3818;
+        int siteId2 = 3823;
+        int workerMUId = 1778;
+        int unitMUId = 4938;
+
+        #region test 001x basic
         [Fact]
-        public void Test0001_MustAlwaysPass()
+        public void Test0001a_MustAlwaysPass()
         {
             //Arrange
             bool checkValueA = true;
             bool checkValueB = false;
 
 
-            //...
             //Act
             checkValueB = true;
 
 
-            //...
             //Assert
             Assert.Equal(checkValueA, checkValueB);
         }
 
         [Fact]
-        public void Test0002_InitiateCore()
+        public void Test0002a_Core_Initiate()
         {
             //Arrange
-            Core checkValueB = null;
+            Core checkValue = null;
 
 
-            //...
             //Act
-            checkValueB = new Core();
+            checkValue = new Core();
 
 
-            //...
             //Assert
-            Assert.NotNull(checkValueB);
+            Assert.NotNull(checkValue);
         }
+
+        [Fact]
+        public void Test0003a_Core_Start_WithBlankExpection()
+        {
+            //Arrange
+            bool checkValueA = true;
+            bool checkValueB = false;
+            Core core = new Core();
+
+
+            //Act
+            try
+            {
+                core.Start("");
+            }
+            catch
+            {
+                checkValueB = true;
+            }
+
+            //Assert
+            Assert.Equal(checkValueA, checkValueB);
+        }
+
+        //[Fact]
+        //public void Test0003b_Core_Start()
+        //{
+        //    //Arrange
+        //    bool checkValueA = true;
+        //    bool checkValueB = false;
+        //    Core core = new Core();
+
+
+        //    //Act
+        //    checkValueB = core.Start(serverConnectionString);
+            
+
+        //    //Assert
+        //    Assert.Equal(checkValueA, checkValueB);
+        //}
+        #endregion
     }
 }
