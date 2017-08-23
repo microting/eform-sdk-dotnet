356705e68be9dbabafab93457ad62a2741bf6b14
TroelsMA <Markvard24@hotmail.com>
2017-08-23 13:33:21 +0200

    Travis fix ?

--- UnitTest/SDK.cs
+++ UnitTest/SDK.cs
@@ -7,10 +7,36 @@ namespace UnitTest
     public class SDK
     {
         [Fact]
-        public void TestMethod1()
+        public void Test0001_MustAlwaysPass()
         {
+            //Arrange
+            bool checkValueA = true;
+            bool checkValueB = false;
+
+
+            //...
+            //Act
+            checkValueB = true;
+
+
+            //...
+            //Assert
+            Assert.Equal(checkValueA, checkValueB);
+        }
+
+        [Fact]
+        public void Test0002_InitiateCore()
+        {
+            //Arrange
             bool value = true;
 
+
+            //...
+            //Act
+
+
+            //...
+            //Assert
             Assert.Equal(true, value);
         }
     }
