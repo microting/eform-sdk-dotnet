8af2330e809277c75f86798f86e4101f3219e83e
René Schultz Madsen <rm@microting.com>
2017-12-28 12:16:09 +0100

    Added DbTestFixture to integration tests

---
+++ eFormSDK.Integration.Tests/DbTestFixture.cs
@@ -0,0 +1,34 @@
+﻿using eFormSqlController;
+using NUnit.Framework;
+
+namespace eFormSDK.Integration.Tests
+{
+    [TestFixture]
+    public abstract class DbTestFixture
+    {
+        protected MicrotingDbMs DbContext;
+
+        [SetUp]
+        public void Setup()
+        {
+            const string connnectionString = @"data source=(LocalDb)\MSSQLLocalDB;Initial catalog=test123";
+
+            DbContext = new MicrotingDbMs(connnectionString);
+            DbContext.Database.CreateIfNotExists();
+
+            DbContext.Database.Initialize(false);
+
+            DoSetup();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            DbContext.Database.Delete();
+            DbContext.Dispose();
+        }
+
+        public virtual void DoSetup() { }
+    }
+
+}
---
+++ eFormSDK.Integration.Tests/SqlControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using eFormSqlController;
+using NUnit.Framework;
+
+namespace eFormSDK.Integration.Tests
+{
+    [TestFixture]
+    public class SqlControllerTest : DbTestFixture
+    {
+        [Test]
+        public void CanPeepTwice()
+        {
+            DbContext.settings.Add(new settings()
+            {
+                id = 1,
+                name = "Test1",
+                value = "SomethingEls2e"
+            });
+
+            DbContext.SaveChanges();
+        }
+    }
+
+}
