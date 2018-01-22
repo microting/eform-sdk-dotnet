a706af7ec8ad7ca7e9d22cc0423a81d2995e80eb
René Schultz Madsen <rm@microting.com>
2018-01-22 14:44:18 +0100

    Claning up a bit.

--- eFormSDK.Integration.Tests/DbTestFixture.cs
+++ eFormSDK.Integration.Tests/DbTestFixture.cs
@@ -1,8 +1,5 @@
 ﻿using eFormSqlController;
-//using Microsoft.WindowsAzure;
 using NUnit.Framework;
-using System;
-//using System.Security.Cryptography.X509Certificates;
 
 namespace eFormSDK.Integration.Tests
 {
@@ -11,12 +8,35 @@ namespace eFormSDK.Integration.Tests
     {
         protected MicrotingDbMs DbContext;
         protected string ConnectionString => @"data source=(LocalDb)\SharedInstance;Initial catalog=eformsdk-tests";
+        private static string authority = "https://login.microsoftonline.com/REPLACE_ME_GUID/oauth2/token";
+        private static string azureSubscriptionId = "__AZURE_SUBSCRIPTION_ID__"; /* Azure Subscription ID */
+
+        private static string location = "Data Center Location"; /* Location of server and location for database (eg. Australia East) */
+        private static string edition = "Standard"; /* Databse Edition (eg. Standard)*/
+        private static string requestedServiceObjectName = "Performance Level"; /* Name of Service Object (eg. S0) */
+
+        private static string resourceGroupName = "Resource Group Name"; /* Name of Resource Group containing SQL Server */
+        private static string serverName = "Server Name"; /* Name of SQL Server */
+        private static string databaseName = "Database Name"; /* Name of Database */
+
+        private static string domainName = "domain.name.com"; /* Tenant ID or AAD domain */
+
+        /* Authentication variables from Azure Active Directory (AAD) */
+        private static string clientId = "__CLIENT_ID__"; /* Active Directory Client ID */
+        private static string clientSecret = "__CLIENT_SECRET__";
+        private static string DirectoryId = "__DIRECTORY_ID__";
+        private static string ApplicationId = "__APPLICATION_ID__";
 
 
         [SetUp]
         public void Setup()
         {
             DbContext = new MicrotingDbMs(ConnectionString);
+
+            if (clientId != "__CLIENT_ID__")
+            {
+
+            }
             DbContext.Database.CreateIfNotExists();
 
             DbContext.Database.Initialize(false);
@@ -28,20 +48,11 @@ namespace eFormSDK.Integration.Tests
         public void TearDown()
         {
             DbContext.Database.Delete();
-            DbContext.Database.CreateIfNotExists();
-
-            DbContext.Database.Initialize(false);
             DbContext.Dispose();
 
         }
 
         public virtual void DoSetup() { }
 
-        //static SubscriptionCloudCredentials getCredentials()
-        //{
-        //    string SubscriptionId = "";
-        //    string CertificateBase64String = "";
-        //    return new CertificateCloudCredentials(SubscriptionId, new X509Certificate2(Convert.FromBase64String(CertificateBase64String))); 
-        //}
     }
 }
