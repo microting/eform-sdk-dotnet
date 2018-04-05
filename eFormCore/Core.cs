7a6c8e8d82d1735357f4998a3c2e6799054cb4f8
René Schultz Madsen <rm@microting.com>
2018-04-05 10:03:38 +0200

    Fixing the caseDelete method.

--- eFormCore/Core.cs
+++ eFormCore/Core.cs
@@ -1204,11 +1204,91 @@ namespace eFormCore
 
         public bool CaseDelete(string microtingUId)
         {
-            bus.SendLocal(new EformDeleteFromServer(microtingUId)).Wait();
+            //bus.SendLocal(new EformDeleteFromServer(microtingUId)).Wait();
             //bus.SendLocal(new EformCompleted(notificationUId, microtingUId)).Wait();
 
+            //string microtingUId = message.MicrotringUUID;
+            string methodName = "CaseDelete";
 
-            return true;
+            log.LogStandard("Not Specified", methodName + " called");
+            log.LogVariable("Not Specified", nameof(microtingUId), microtingUId);
+
+            var cDto = sqlController.CaseReadByMUId(microtingUId);
+            string xmlResponse = communicator.Delete(microtingUId, cDto.SiteUId);
+            Response resp = new Response();
+
+            if (xmlResponse.Contains("Error occured: Contact Microting"))
+            {
+                log.LogEverything("Not Specified", "XML response:");
+                log.LogEverything("Not Specified", xmlResponse);
+                log.LogEverything("DELETE ERROR", methodName + " failed for microtingUId: " + microtingUId);
+                return false;
+            }
+
+            if (xmlResponse.Contains("Error"))
+            {
+                try
+                {
+                    resp = resp.XmlToClass(xmlResponse);
+                    log.LogException("Not Specified", methodName + " failed", new Exception("Error from Microting server: " + resp.Value), false);
+                    return false;
+                }
+                catch (Exception ex)
+                {
+                    try
+                    {
+                        log.LogException("Not Specified", methodName + " (string " + microtingUId + ") failed", ex, false);
+                    }
+                    catch
+                    {
+                        log.LogException("Not Specified", methodName + " (string microtingUId) failed", ex, false);
+                    }
+                    return false;
+                }
+            }
+
+            if (xmlResponse.Contains("Parsing in progress: Can not delete check list!</Value>"))
+                for (int i = 1; i < 7; i++)
+                {
+                    Thread.Sleep(i * 200);
+                    xmlResponse = communicator.Delete(microtingUId, cDto.SiteUId);
+                    if (!xmlResponse.Contains("Parsing in progress: Can not delete check list!</Value>"))
+                        break;
+                }
+
+            log.LogEverything("Not Specified", "XML response:");
+            log.LogEverything("Not Specified", xmlResponse);
+
+            resp = resp.XmlToClass(xmlResponse);
+            if (resp.Type.ToString() == "Success")
+            {
+                try
+                {
+                    sqlController.CaseDelete(microtingUId);
+
+                    cDto = sqlController.CaseReadByMUId(microtingUId);
+                    FireHandleCaseDeleted(cDto);
+
+                    log.LogStandard("Not Specified", cDto.ToString() + " has been removed");
+
+                    return true;
+                }
+                catch { }
+
+                try
+                {
+                    sqlController.CaseDeleteReversed(microtingUId);
+
+                    cDto = sqlController.CaseReadByMUId(microtingUId);
+                    FireHandleCaseDeleted(cDto);
+                    log.LogStandard("Not Specified", cDto.ToString() + " has been removed");
+
+                    return true;
+                }
+                catch { }
+            }
+            return false;
+            //return true;
         }
 
         public bool CaseDeleteResult(int caseId)
