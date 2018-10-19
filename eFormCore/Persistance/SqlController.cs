a9d921a4e88e9d65990a7721dc8195db6616295a
René Schultz Madsen <rm@microting.com>
2018-10-19 13:42:00 +0200

    Fixing typo in Int.parse to int.parse

--- eFormCore/Persistance/SqlController.cs
+++ eFormCore/Persistance/SqlController.cs
@@ -2621,7 +2621,7 @@ namespace eFormSqlController
                             {
                                 if (item.value != "" || item.value != null)
                                 {
-                                    entity_items match = db.entity_items.SingleOrDefault(x => x.id == Int.parse(item.value));
+                                    entity_items match = db.entity_items.SingleOrDefault(x => x.id == int.parse(item.value));
 
                                     if (match != null)
                                     {
