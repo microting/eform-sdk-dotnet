18a857a0a57add3d3362a83162a1a0a5d7e43056
René Schultz Madsen <rm@microting.com>
2018-10-19 11:53:52 +0200

    Fixing the broken read/update of entity_item on cases.

--- eFormCore/Persistance/SqlController.cs
+++ eFormCore/Persistance/SqlController.cs
@@ -1595,7 +1595,7 @@ namespace eFormSqlController
                                 if (match != null)
                                 {
                                     field_value.ValueReadable = match.name;
-                                    field_value.Value = match.entity_item_uid;
+                                    field_value.Value = match.id.ToString();
                                     field_value.MicrotingUuid = match.microting_uid;
                                 }
 
@@ -2636,7 +2636,7 @@ namespace eFormSqlController
                             {
                                 if (item.value != "" || item.value != null)
                                 {
-                                    entity_items match = db.entity_items.SingleOrDefault(x => x.microting_uid == item.value);
+                                    entity_items match = db.entity_items.SingleOrDefault(x => x.id == int.Parse(item.value));
 
                                     if (match != null)
                                     {
