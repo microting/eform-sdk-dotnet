b711e27bc5542c14616ba57cde2b1f8688519733
René Schultz Madsen <rm@microting.dk>
2019-01-21 11:09:40 +0100

    Moving Windows platforms to use MySQL also.

--- eFormSDK.Integration.Tests/DbTestFixture.cs
+++ eFormSDK.Integration.Tests/DbTestFixture.cs
@@ -38,14 +38,14 @@ namespace eFormSDK.Integration.Tests
         public void Setup()
         {
 
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                ConnectionString = @"data source=(LocalDb)\SharedInstance;Initial catalog=eformsdk-tests;Integrated Security=True";
-            }
-            else
-            {
+//            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+//            {
+//                ConnectionString = @"data source=(LocalDb)\SharedInstance;Initial catalog=eformsdk-tests;Integrated Security=True";
+//            }
+//            else
+//            {
                 ConnectionString = @"Server = localhost; port = 3306; Database = eformsdk-tests; user = root; Convert Zero Datetime = true;";
-            }
+//            }
 
             DbContext = GetContext(ConnectionString);
 
