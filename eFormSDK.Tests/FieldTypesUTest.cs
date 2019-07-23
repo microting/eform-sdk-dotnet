8a2a48e15039db34bd2f36b9144d271b5622e6d1
thejokerd3 <muslimalali@hotmail.com>
2019-07-23 13:29:37 +0200

    working On FieldTypeTest

---
+++ eFormSDK.Tests/FieldTypesUTest.cs
@@ -0,0 +1,26 @@
+using System;
+using Microting.eForm.Infrastructure.Data.Entities;
+using NUnit.Framework;
+
+namespace eFormSDK.Tests
+{
+    [TestFixture]
+    public class FieldTypesUTest : DbTestFixture
+    {
+        [Test]
+        public void FieldType_Create_DoesCreate()
+        {
+            //Arrange
+            
+            Random rnd = new Random();
+            
+            field_types fieldType = new field_types();
+            fieldType.Description = Guid.NewGuid().ToString();
+            fieldType.Id = rnd.Next(1, 255);
+            fieldType.FieldType = Guid.NewGuid().ToString();
+            
+            //TODO 
+            
+        }
+    }
+}
