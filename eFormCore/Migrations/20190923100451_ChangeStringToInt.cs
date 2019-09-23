7c5c2e291a4df67e92cdb3eb458e34a86c3dec78
René Schultz Madsen <rm@microting.dk>
2019-09-23 12:06:01 +0200

    Adding changes to have the code migrate ids from string to int.

---
+++ eFormCore/Migrations/20190923100451_ChangeStringToInt.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Microting.eForm.Migrations
+{
+    public partial class ChangeStringToInt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "MicrotingUid",
+                table: "notifications",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 255,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MicrotingUid",
+                table: "notification_versions",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 255,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MicrotingUid",
+                table: "check_list_sites",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 255,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LastCheckId",
+                table: "check_list_sites",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 255,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MicrotingUid",
+                table: "check_list_site_versions",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 255,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LastCheckId",
+                table: "check_list_site_versions",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 255,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MicrotingUid",
+                table: "cases",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 255,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MicrotingCheckUid",
+                table: "cases",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 255,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MicrotingUid",
+                table: "case_versions",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 255,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MicrotingCheckUid",
+                table: "case_versions",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 255,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MicrotingUid",
+                table: "notifications",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MicrotingUid",
+                table: "notification_versions",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MicrotingUid",
+                table: "check_list_sites",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastCheckId",
+                table: "check_list_sites",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MicrotingUid",
+                table: "check_list_site_versions",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastCheckId",
+                table: "check_list_site_versions",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MicrotingUid",
+                table: "cases",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MicrotingCheckUid",
+                table: "cases",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MicrotingUid",
+                table: "case_versions",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MicrotingCheckUid",
+                table: "case_versions",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(int),
+                oldNullable: true);
+        }
+    }
+}
