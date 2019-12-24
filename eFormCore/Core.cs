25650b0873261f33c741a001971841ff1dd903dd
René Schultz Madsen <rm@microting.dk>
2019-12-24 09:24:14 +0100

    Starting moving code around.

--- eFormCore/Core.cs
+++ eFormCore/Core.cs
@@ -52,6 +52,7 @@ using Microting.eForm.Helpers;
 using Microting.eForm.Infrastructure;
 using Microting.eForm.Infrastructure.Constants;
 using Microting.eForm.Infrastructure.Data.Entities;
+using Microting.eForm.Infrastructure.Helpers;
 using Microting.eForm.Infrastructure.Models;
 using Microting.eForm.Infrastructure.Models.reply;
 using Microting.eForm.Installers;
@@ -82,6 +83,7 @@ namespace eFormCore
         private Subscriber _subscriber;
         private Communicator _communicator;
         private SqlController _sqlController;
+        private DbContextHelper dbContextHelper;
         private readonly Tools t = new Tools();
 
         private IWindsorContainer _container;
@@ -213,6 +215,8 @@ namespace eFormCore
                     if (string.IsNullOrEmpty(connectionString))
                         throw new ArgumentException("serverConnectionString is not allowed to be null or empty");
 
+                    dbContextHelper = new DbContextHelper(connectionString);
+                    
                     //sqlController
                     _sqlController = new SqlController(connectionString);
 
@@ -254,6 +258,7 @@ namespace eFormCore
 
                     _container = new WindsorContainer();
                     _container.Register(Component.For<SqlController>().Instance(_sqlController));
+                    _container.Register(Component.For<DbContextHelper>().Instance(dbContextHelper));
                     _container.Register(Component.For<Communicator>().Instance(_communicator));
                     _container.Register(Component.For<Log>().Instance(log));
                     _container.Register(Component.For<Core>().Instance(this));
@@ -689,7 +694,7 @@ namespace eFormCore
                         throw new ArgumentNullException(nameof(mainElement), "mainElement not allowed to be null");
 
                     List<string> errorList = await FieldValidation(mainElement);
-                    errorList = errorList.Concat(await CheckListValidation(mainElement)).ToList();
+                    errorList = errorList.Concat(await ValidationHelper.CheckListValidation(mainElement)).ToList();
 
                     return errorList;
 
@@ -706,83 +711,9 @@ namespace eFormCore
 
         private async Task<List<string>> FieldValidation(MainElement mainElement)
         {
-            string methodName = "Core.FieldValidation";
-
-            await log.LogStandard(methodName, "called");
-
-            List<string> errorLst = new List<string>();
-            var dataItems = mainElement.DataItemGetAll();
-
-            foreach (var dataItem in dataItems)
-            {
-                #region entities
-
-                if (dataItem.GetType() == typeof(EntitySearch))
-                {
-                    EntitySearch entitySearch = (EntitySearch) dataItem;
-                    var temp = _sqlController.EntityGroupRead(entitySearch.EntityTypeId.ToString());
-                    if (temp == null)
-                        errorLst.Add("Element entitySearch.EntityTypeId:'" + entitySearch.EntityTypeId +
-                                     "' is an reference to a local unknown EntitySearch group. Please update reference");
-                }
-
-                if (dataItem.GetType() == typeof(EntitySelect))
-                {
-                    EntitySelect entitySelect = (EntitySelect) dataItem;
-                    var temp = _sqlController.EntityGroupRead(entitySelect.Source.ToString());
-                    if (temp == null)
-                        errorLst.Add("Element entitySelect.Source:'" + entitySelect.Source +
-                                     "' is an reference to a local unknown EntitySearch group. Please update reference");
-                }
-
-                #endregion
-
-                #region PDF
-
-                if (dataItem.GetType() == typeof(ShowPdf))
-                {
-                    ShowPdf showPdf = (ShowPdf) dataItem;
-                    errorLst.AddRange(await PdfValidate(showPdf.Value, showPdf.Id));
-                }
-                
-                List<string> acceptedColors = new List<string>();
-                acceptedColors.Add(Constants.FieldColors.Grey);
-                acceptedColors.Add(Constants.FieldColors.Red);
-                acceptedColors.Add(Constants.FieldColors.Green);
-                acceptedColors.Add(Constants.FieldColors.Blue);
-                acceptedColors.Add(Constants.FieldColors.Purple);
-                acceptedColors.Add(Constants.FieldColors.Yellow);
-                acceptedColors.Add(Constants.FieldColors.Default);
-                acceptedColors.Add(Constants.FieldColors.None);
-                if (!acceptedColors.Contains(dataItem.Color) && !string.IsNullOrEmpty(dataItem.Color))
-                {
-                    errorLst.Add($"DataItem with label {dataItem.Label} did supply color {dataItem.Color}, but the only allowed values are: e8eaf6 for grey, ffe4e4 for red, f0f8db for green, e2f4fb for blue, e2f4fb for purple, fff6df for yellow, None for default or leave it blank.");
-                }
-
-                #endregion
+            using (MicrotingDbContext dbContext = dbContextHelper.GetDbContext()) {
+                return await ValidationHelper.FieldValidation(dbContext, mainElement);
             }
-
-            return errorLst;
-
-        }
-
-        private async Task<List<string>> CheckListValidation(MainElement mainElement)
-        {
-            string methodName = "Core.CheckListValidation";
-            await log.LogStandard(methodName, "called");
-            List<string> errorLst = new List<string>();
-            
-            List<string> acceptedColors = new List<string>();
-            acceptedColors.Add(Constants.CheckListColors.Grey);
-            acceptedColors.Add(Constants.CheckListColors.Red);
-            acceptedColors.Add(Constants.CheckListColors.Green);
-
-            if (!acceptedColors.Contains(mainElement.Color) && !string.IsNullOrEmpty(mainElement.Color))
-            {
-                errorLst.Add($"mainElement with label {mainElement.Label} did supply color {mainElement.Color}, but the only allowed colors are: grey, red, green or leave it blank.");
-            }
-            
-            return errorLst;
         }
 
         public async Task<MainElement> TemplateUploadData(MainElement mainElement)
--- eFormCore/Helpers/ValidationHelper.cs
+++ eFormCore/Helpers/ValidationHelper.cs
@@ -1,91 +1,112 @@
-//using System.Collections.Generic;
-//using System.Threading.Tasks;
-//using Microting.eForm.Infrastructure.Models;
-//
-//namespace Microting.eForm.Helpers
-//{
-//    public static class ValidationHelper
-//    {
-//    
-//        private async Task<List<string>> FieldValidation(MainElement mainElement)
-//        {
-//            string methodName = "Core.FieldValidation";
-//
-//            await log.LogStandard(methodName, "called");
-//
-//            List<string> errorLst = new List<string>();
-//            var dataItems = mainElement.DataItemGetAll();
-//
-//            foreach (var dataItem in dataItems)
-//            {
-//                #region entities
-//
-//                if (dataItem.GetType() == typeof(EntitySearch))
-//                {
-//                    EntitySearch entitySearch = (EntitySearch) dataItem;
-//                    var temp = _sqlController.EntityGroupRead(entitySearch.EntityTypeId.ToString());
-//                    if (temp == null)
-//                        errorLst.Add("Element entitySearch.EntityTypeId:'" + entitySearch.EntityTypeId +
-//                                     "' is an reference to a local unknown EntitySearch group. Please update reference");
-//                }
-//
-//                if (dataItem.GetType() == typeof(EntitySelect))
-//                {
-//                    EntitySelect entitySelect = (EntitySelect) dataItem;
-//                    var temp = _sqlController.EntityGroupRead(entitySelect.Source.ToString());
-//                    if (temp == null)
-//                        errorLst.Add("Element entitySelect.Source:'" + entitySelect.Source +
-//                                     "' is an reference to a local unknown EntitySearch group. Please update reference");
-//                }
-//
-//                #endregion
-//
-//                #region PDF
-//
-//                if (dataItem.GetType() == typeof(ShowPdf))
-//                {
-//                    ShowPdf showPdf = (ShowPdf) dataItem;
-//                    errorLst.AddRange(await PdfValidate(showPdf.Value, showPdf.Id));
-//                }
-//                
-//                List<string> acceptedColors = new List<string>();
-//                acceptedColors.Add(Constants.FieldColors.Grey);
-//                acceptedColors.Add(Constants.FieldColors.Red);
-//                acceptedColors.Add(Constants.FieldColors.Green);
-//                acceptedColors.Add(Constants.FieldColors.Blue);
-//                acceptedColors.Add(Constants.FieldColors.Purple);
-//                acceptedColors.Add(Constants.FieldColors.Yellow);
-//                acceptedColors.Add(Constants.FieldColors.Default);
-//                acceptedColors.Add(Constants.FieldColors.None);
-//                if (!acceptedColors.Contains(dataItem.Color) && !string.IsNullOrEmpty(dataItem.Color))
-//                {
-//                    errorLst.Add($"DataItem with label {dataItem.Label} did supply color {dataItem.Color}, but the only allowed values are: e8eaf6 for grey, ffe4e4 for red, f0f8db for green, e2f4fb for blue, e2f4fb for purple, fff6df for yellow, None for default or leave it blank.");
-//                }
-//
-//                #endregion
-//            }
-//
-//            return errorLst;
-//
-//        }
-//    
-//        private async Task<List<string>> CheckListValidation(MainElement mainElement)
-//        {
-//            string methodName = "Core.CheckListValidation";
-//            await log.LogStandard(methodName, "called");
-//            List<string> errorLst = new List<string>();
-//            
-//            List<string> acceptedColors = new List<string>();
-//            acceptedColors.Add(Constants.CheckListColors.Grey);
-//            acceptedColors.Add(Constants.CheckListColors.Red);
-//            acceptedColors.Add(Constants.CheckListColors.Green);
-//
-//            if (!acceptedColors.Contains(mainElement.Color) && !string.IsNullOrEmpty(mainElement.Color))
-//            {
-//                errorLst.Add($"mainElement with label {mainElement.Label} did supply color {mainElement.Color}, but the only allowed colors are: grey, red, green or leave it blank.");
-//            }
-//            
-//            return errorLst;
-//        }
-//    }
-//}
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microting.eForm.Infrastructure;
+using Microting.eForm.Infrastructure.Constants;
+using Microting.eForm.Infrastructure.Data.Entities;
+using Microting.eForm.Infrastructure.Models;
+
+namespace Microting.eForm.Helpers
+{
+    public static class ValidationHelper
+    {
+        
+        public static async Task<List<string>> FieldValidation(MicrotingDbContext dbContext, MainElement mainElement)
+        {
+            string methodName = "Core.FieldValidation";
+
+            //await log.LogStandard(methodName, "called");
+
+            List<string> errorLst = new List<string>();
+            var dataItems = mainElement.DataItemGetAll();
+
+            foreach (var dataItem in dataItems)
+            {
+                #region entities
+
+                if (dataItem.GetType() == typeof(EntitySearch))
+                {
+                    EntitySearch entitySearch = (EntitySearch) dataItem;
+                    var temp = await entity_groups.ReadSorted(dbContext, entitySearch.EntityTypeId.ToString(), "Id", "");// _sqlController.EntityGroupRead(entitySearch.EntityTypeId.ToString());
+                    if (temp == null)
+                        errorLst.Add("Element entitySearch.EntityTypeId:'" + entitySearch.EntityTypeId +
+                                     "' is an reference to a local unknown EntitySearch group. Please update reference");
+                }
+
+                if (dataItem.GetType() == typeof(EntitySelect))
+                {
+                    EntitySelect entitySelect = (EntitySelect) dataItem;
+                    var temp = await entity_groups.ReadSorted(dbContext, entitySelect.Source.ToString(), "Id", "");//_sqlController.EntityGroupRead(entitySelect.Source.ToString());
+                    if (temp == null)
+                        errorLst.Add("Element entitySelect.Source:'" + entitySelect.Source +
+                                     "' is an reference to a local unknown EntitySearch group. Please update reference");
+                }
+
+                #endregion
+
+                #region PDF
+
+                if (dataItem.GetType() == typeof(ShowPdf))
+                {
+                    ShowPdf showPdf = (ShowPdf) dataItem;
+                    errorLst.AddRange(await PdfValidate(showPdf.Value, showPdf.Id));
+                }
+                
+                List<string> acceptedColors = new List<string>();
+                acceptedColors.Add(Constants.FieldColors.Grey);
+                acceptedColors.Add(Constants.FieldColors.Red);
+                acceptedColors.Add(Constants.FieldColors.Green);
+                acceptedColors.Add(Constants.FieldColors.Blue);
+                acceptedColors.Add(Constants.FieldColors.Purple);
+                acceptedColors.Add(Constants.FieldColors.Yellow);
+                acceptedColors.Add(Constants.FieldColors.Default);
+                acceptedColors.Add(Constants.FieldColors.None);
+                if (!acceptedColors.Contains(dataItem.Color) && !string.IsNullOrEmpty(dataItem.Color))
+                {
+                    errorLst.Add($"DataItem with label {dataItem.Label} did supply color {dataItem.Color}, but the only allowed values are: e8eaf6 for grey, ffe4e4 for red, f0f8db for green, e2f4fb for blue, e2f4fb for purple, fff6df for yellow, None for default or leave it blank.");
+                }
+
+                #endregion
+            }
+
+            return errorLst;
+
+        }
+        
+        private static async Task<List<string>> PdfValidate(string pdfString, int pdfId)
+        {
+            await Task.Run(() => { }); // TODO FIX ME
+            List<string> errorLst = new List<string>();
+
+            if (pdfString.ToLower().Contains("microting.com"))
+                errorLst.Add("Element showPdf.Id:'" + pdfId + "' contains an URL that points to Microting's builder temporary hosting. Indicating that it's not a proper hash");
+            if (pdfString.ToLower().Contains("http") || pdfString.ToLower().Contains("https"))
+                errorLst.Add("Element showPdf.Id:'" + pdfId + "' contains an HTTP or HTTPS. Indicating that it's not a proper hash");
+            if (pdfString.Length != 32)
+                errorLst.Add("Element showPdf.Id:'" + pdfId + "' length is not the correct length (32). Indicating that it's not a proper hash");
+
+            if (errorLst.Count > 0)
+                errorLst.Add("Element showPdf.Id:'" + pdfId + "' please check 'value' input, and consider running PdfUpload");
+
+            return errorLst;
+        }
+        
+        public static async Task<List<string>> CheckListValidation(MainElement mainElement)
+        {
+            string methodName = "Core.CheckListValidation";
+            //await log.LogStandard(methodName, "called");
+            List<string> errorLst = new List<string>();
+            
+            List<string> acceptedColors = new List<string>();
+            acceptedColors.Add(Constants.CheckListColors.Grey);
+            acceptedColors.Add(Constants.CheckListColors.Red);
+            acceptedColors.Add(Constants.CheckListColors.Green);
+
+            if (!acceptedColors.Contains(mainElement.Color) && !string.IsNullOrEmpty(mainElement.Color))
+            {
+                errorLst.Add($"mainElement with label {mainElement.Label} did supply color {mainElement.Color}, but the only allowed colors are: grey, red, green or leave it blank.");
+            }
+            
+            return errorLst;
+        }
+    }
+}
