6460d450e5e12dae1ffd174ae4b562d5f546da52
René Schultz Madsen <rm@microting.dk>
2020-02-22 11:15:45 +0100

    Adding changes to make it use dot net 3.0

--- armprepareinstall.sh
+++ armprepareinstall.sh
@@ -12,12 +12,12 @@ if [ $ARCH = $ARMARCH ]; then
 	echo $PATH
 	ls -lah /usr/share/dotnet
 else
-	wget -q https://packages.microsoft.com/config/ubuntu/19.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-	sudo dpkg -i packages-microsoft-prod.deb
-	sudo apt-get update
-	sudo apt-get install apt-transport-https
-	sudo apt-get update
-	sudo apt-get install -qq dotnet-sdk-3.0
-	echo "WE ARE NOT ON ARM"
-	echo $ARCH
+	#wget -q https://packages.microsoft.com/config/ubuntu/19.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+	#sudo dpkg -i packages-microsoft-prod.deb
+	#sudo apt-get update
+	#sudo apt-get install apt-transport-https
+	#sudo apt-get update
+	#sudo apt-get install -qq dotnet-sdk-3.0
+	#echo "WE ARE NOT ON ARM"
+	#echo $ARCH
 fi
