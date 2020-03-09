e15719819ad8018d2b6532115e60bc5e343d5ee0
René Schultz Madsen <rm@microting.com>
2020-03-09 09:54:17 +0100

    Trying to add --logger:Console

--- unit-tests.sh
+++ unit-tests.sh
@@ -2,4 +2,4 @@
 ./armprepareinstall.sh
 dotnet restore
 dotnet build --no-restore eFormSDK.sln
-dotnet test --no-restore -c Release -v n eFormSDK.Tests/eFormSDK.Tests.csproj || travis_terminate 1;
+dotnet test --no-restore -c Release -v n eFormSDK.Tests/eFormSDK.Tests.csproj --logger:Console || travis_terminate 1;
