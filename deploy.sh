a49df75989beb2b8a48cf50bc4ff6526b6edbe7a
René Schultz Madsen <rm@microting.dk>
2020-06-20 19:43:21 +0200

    Fixing the path for the nupkg

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ dotnet build --no-restore eFormSDK.sln
 export GITVERSION=`git describe --abbrev=0 --tags | cut -d "v" -f 2`
 echo $GITVERSION
 dotnet pack eFormSDK.sln -c Release -o ./artifacts -p:PackageVersion=$GITVERSION
-export FILENAME="/home/travis/build/microting/eform-sdk-dotnet/eFormCore/artifacts/Microting.eForm.${GITVERSION}.nupkg"
+export FILENAME="/home/travis/build/microting/eform-sdk-dotnet/artifacts/Microting.eForm.${GITVERSION}.nupkg"
 echo $FILENAME
 dotnet nuget push $FILENAME -k $NUGET_SECRET_KEY -s https://api.nuget.org/v3/index.json || true
