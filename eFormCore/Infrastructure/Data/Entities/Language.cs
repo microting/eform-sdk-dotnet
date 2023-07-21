2c9e6ae6fd8e49de9a608101f2a3b9a3841d1182
René Schultz Madsen <rm@microting.dk>
2023-07-21 19:35:48 +0200

    Fixing languages and their names. Adding Türkçe

--- eFormCore/Infrastructure/Data/Entities/Language.cs
+++ eFormCore/Infrastructure/Data/Entities/Language.cs
@@ -37,11 +37,18 @@ namespace Microting.eForm.Infrastructure.Data.Entities
 
         public static async Task AddDefaultLanguages(MicrotingDbContext dbContext)
         {
-            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Danish") == null)
+
+            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Danish") != null)
+            {
+                Language language = dbContext.Languages.First(x => x.Name == "Danish");
+                language.Name = "Dansk";
+                await language.Update(dbContext);
+            }
+            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Dansk") == null)
             {
                 Language language = new Language
                 {
-                    Name = "Danish",
+                    Name = "Dansk",
                     LanguageCode = "da",
                     IsActive = true
                 };
@@ -59,133 +66,228 @@ namespace Microting.eForm.Infrastructure.Data.Entities
                 await language.Create(dbContext);
             }
 
-            if (dbContext.Languages.FirstOrDefault(x => x.Name == "German") == null)
+            if (dbContext.Languages.FirstOrDefault(x => x.Name == "German") != null)
+            {
+                Language language = dbContext.Languages.First(x => x.Name == "German");
+                language.Name = "Deutsch";
+                await language.Update(dbContext);
+            }
+
+            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Deutsch") == null)
             {
                 Language language = new Language
                 {
-                    Name = "German",
+                    Name = "Deutsch",
                     LanguageCode = "de-DE",
                     IsActive = true
                 };
                 await language.Create(dbContext);
             }
 
-            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Ukrainian") == null)
+            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Ukrainian") != null)
+            {
+                Language language = dbContext.Languages.First(x => x.Name == "Ukrainian");
+                language.Name = "українська";
+                await language.Update(dbContext);
+            }
+
+            if (dbContext.Languages.FirstOrDefault(x => x.Name == "українська") == null)
             {
                 Language language = new Language
                 {
-                    Name = "Ukrainian",
+                    Name = "українська",
                     LanguageCode = "uk-UA",
                     IsActive = false
                 };
                 await language.Create(dbContext);
             }
 
-            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Polish") == null)
+            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Polish") != null)
+            {
+                Language language = dbContext.Languages.First(x => x.Name == "Polish");
+                language.Name = "Polsk";
+                await language.Update(dbContext);
+            }
+
+            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Polski") == null)
             {
                 Language language = new Language
                 {
-                    Name = "Polish",
+                    Name = "Polski",
                     LanguageCode = "pl-PL",
                     IsActive = false
                 };
                 await language.Create(dbContext);
             }
 
-            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Norwegian") == null)
+            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Norwegian") != null)
+            {
+                Language language = dbContext.Languages.First(x => x.Name == "Norwegian");
+                language.Name = "Norsk";
+                await language.Update(dbContext);
+            }
+
+            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Norsk") == null)
             {
                 Language language = new Language
                 {
-                    Name = "Norwegian",
+                    Name = "Norsk",
                     LanguageCode = "no-NO",
                     IsActive = false
                 };
                 await language.Create(dbContext);
             }
 
-            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Swedish") == null)
+            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Swedish") != null)
+            {
+                Language language = dbContext.Languages.First(x => x.Name == "Swedish");
+                language.Name = "Svenska";
+                await language.Update(dbContext);
+            }
+
+            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Svenska") == null)
             {
                 Language language = new Language
                 {
-                    Name = "Swedish",
+                    Name = "Svenska",
                     LanguageCode = "sv-SE",
                     IsActive = false
                 };
                 await language.Create(dbContext);
             }
 
-            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Spanish") == null)
+            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Spanish") != null)
+            {
+                Language language = dbContext.Languages.First(x => x.Name == "Spanish");
+                language.Name = "Español";
+                await language.Update(dbContext);
+            }
+
+            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Español") == null)
             {
                 Language language = new Language
                 {
-                    Name = "Spanish",
+                    Name = "Española",
                     LanguageCode = "es-ES",
                     IsActive = false
                 };
                 await language.Create(dbContext);
             }
 
-            if (dbContext.Languages.FirstOrDefault(x => x.Name == "French") == null)
+            if (dbContext.Languages.FirstOrDefault(x => x.Name == "French") != null)
+            {
+                Language language = dbContext.Languages.First(x => x.Name == "French");
+                language.Name = "Français";
+                await language.Update(dbContext);
+            }
+
+            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Français") == null)
             {
                 Language language = new Language
                 {
-                    Name = "French",
+                    Name = "Français",
                     LanguageCode = "fr-FR",
                     IsActive = false
                 };
                 await language.Create(dbContext);
             }
 
-            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Italian") == null)
+            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Italian") != null)
+            {
+                Language language = dbContext.Languages.First(x => x.Name == "Italian");
+                language.Name = "Italiana";
+                await language.Update(dbContext);
+            }
+
+            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Italiana") == null)
             {
                 Language language = new Language
                 {
-                    Name = "Italian",
+                    Name = "Italiana",
                     LanguageCode = "it-IT",
                     IsActive = false
                 };
                 await language.Create(dbContext);
             }
 
-            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Dutch") == null)
+            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Dutch") != null)
+            {
+                Language language = dbContext.Languages.First(x => x.Name == "Dutch");
+                language.Name = "Neerlandais";
+                await language.Update(dbContext);
+            }
+
+            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Neerlandais") == null)
             {
                 Language language = new Language
                 {
-                    Name = "Dutch",
+                    Name = "Neerlandais",
                     LanguageCode = "nl-NL",
                     IsActive = false
                 };
                 await language.Create(dbContext);
             }
 
-            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Brazilian Portuguese") == null)
+            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Brazilian Portuguese") != null)
+            {
+                Language language = dbContext.Languages.First(x => x.Name == "Brazilian Portuguese");
+                language.Name = "Portugues do Brasil";
+                await language.Update(dbContext);
+            }
+
+            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Portugues do Brasil") == null)
             {
                 Language language = new Language
                 {
-                    Name = "Brazilian Portuguese",
+                    Name = "Portugues do Brasil",
                     LanguageCode = "pt-BR",
                     IsActive = false
                 };
                 await language.Create(dbContext);
             }
 
-            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Portuguese") == null)
+            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Portuguese") != null)
+            {
+                Language language = dbContext.Languages.First(x => x.Name == "Portuguese");
+                language.Name = "Português";
+                await language.Update(dbContext);
+            }
+
+            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Português") == null)
             {
                 Language language = new Language
                 {
-                    Name = "Portuguese",
+                    Name = "Português",
                     LanguageCode = "pt-PT",
                     IsActive = false
                 };
                 await language.Create(dbContext);
             }
 
-            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Finish") == null)
+            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Finish") != null)
+            {
+                Language language = dbContext.Languages.First(x => x.Name == "Finish");
+                language.Name = "Suomalainen";
+                await language.Update(dbContext);
+            }
+
+            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Suomalainen") == null)
             {
                 Language language = new Language
                 {
-                    Name = "Finish",
+                    Name = "Suomalainen",
                     LanguageCode = "fi-FI",
+                    IsActive = false
+                };
+                await language.Create(dbContext);
+            }
+
+            if (dbContext.Languages.FirstOrDefault(x => x.Name == "Türkçe") == null)
+            {
+                Language language = new Language
+                {
+                    Name = "Türkçe",
+                    LanguageCode = "tr-TR",
                     IsActive = false
                 };
                 await language.Create(dbContext);
