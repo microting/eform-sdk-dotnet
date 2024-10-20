0fbdea743e5bf6f6705f25b60a1cba5fc1172c9b
René Schultz Madsen <rm@microting.dk>
2024-10-20 11:13:24 +0200

    Disabling profiling.

--- eFormCore/Core.cs
+++ eFormCore/Core.cs
@@ -272,12 +272,12 @@ namespace eFormCore
                 // Requires NuGet package: Sentry.Profiling
                 // Note: By default, the profiler is initialized asynchronously. This can
                 // be tuned by passing a desired initialization timeout to the constructor.
-                options.AddIntegration(new ProfilingIntegration(
-                    // During startup, wait up to 500ms to profile the app startup code.
-                    // This could make launching the app a bit slower so comment it out if you
-                    // prefer profiling to start asynchronously
-                    TimeSpan.FromMilliseconds(500)
-                ));
+                // options.AddIntegration(new ProfilingIntegration(
+                //     // During startup, wait up to 500ms to profile the app startup code.
+                //     // This could make launching the app a bit slower so comment it out if you
+                //     // prefer profiling to start asynchronously
+                //     TimeSpan.FromMilliseconds(500)
+                // ));
             });
             string methodName = "Core.StartSqlOnly";
             try
