b45b42629882f56e3b4472a0935107907145460e
René Schultz Madsen <rm@microting.com>
2024-10-20 09:35:24 +0200

    Adding Sentry to catch exceptions.

--- eFormCore/Core.cs
+++ eFormCore/Core.cs
@@ -62,6 +62,8 @@ using Microting.eForm.Services;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Rebus.Bus;
+using Sentry;
+using Sentry.Profiling;
 using Case = Microting.eForm.Dto.Case;
 using CheckListValue = Microting.eForm.Infrastructure.Models.CheckListValue;
 using DataItem = Microting.eForm.Infrastructure.Models.DataItem;
@@ -242,6 +244,41 @@ namespace eFormCore
 
         public async Task<bool> StartSqlOnly(string connectionString)
         {
+            SentrySdk.Init(options =>
+            {
+                // A Sentry Data Source Name (DSN) is required.
+                // See https://docs.sentry.io/product/sentry-basics/dsn-explainer/
+                // You can set it in the SENTRY_DSN environment variable, or you can set it in code here.
+                options.Dsn = "https://2ada2ec6fccf69ac9591a456b0544b4e@o4506241219428352.ingest.us.sentry.io/4508153901678592";
+
+                // When debug is enabled, the Sentry client will emit detailed debugging information to the console.
+                // This might be helpful, or might interfere with the normal operation of your application.
+                // We enable it here for demonstration purposes when first trying Sentry.
+                // You shouldn't do this in your applications unless you're troubleshooting issues with Sentry.
+                options.Debug = true;
+
+                // This option is recommended. It enables Sentry's "Release Health" feature.
+                options.AutoSessionTracking = true;
+
+                // Set TracesSampleRate to 1.0 to capture 100%
+                // of transactions for tracing.
+                // We recommend adjusting this value in production.
+                options.TracesSampleRate = 1.0;
+
+                // Sample rate for profiling, applied on top of othe TracesSampleRate,
+                // e.g. 0.2 means we want to profile 20 % of the captured transactions.
+                // We recommend adjusting this value in production.
+                options.ProfilesSampleRate = 1.0;
+                // Requires NuGet package: Sentry.Profiling
+                // Note: By default, the profiler is initialized asynchronously. This can
+                // be tuned by passing a desired initialization timeout to the constructor.
+                options.AddIntegration(new ProfilingIntegration(
+                    // During startup, wait up to 500ms to profile the app startup code.
+                    // This could make launching the app a bit slower so comment it out if you
+                    // prefer profiling to start asynchronously
+                    TimeSpan.FromMilliseconds(500)
+                ));
+            });
             string methodName = "Core.StartSqlOnly";
             try
             {
