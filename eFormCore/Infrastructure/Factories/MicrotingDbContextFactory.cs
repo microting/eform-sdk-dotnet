9121d3f7fc9e35dc42190b02728e35714166e3c0
René Schultz Madsen <rm@microting.dk>
2026-04-21 13:46:35 +0200

    Updating all dependencies to latest version

--- eFormCore/Infrastructure/Factories/MicrotingDbContextFactory.cs
+++ eFormCore/Infrastructure/Factories/MicrotingDbContextFactory.cs
@@ -22,11 +22,9 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-using System;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 
 namespace Microting.eForm.Infrastructure.Factories;
 
